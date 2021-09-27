7c6e907257e787efbcb55edc91027b2b3541316b
JackLinnehan <linnehjb@bc.edu>
2021-09-27 12:04:51 -0400

    5.6 toDoLists Structs

---
+++ ToDoList/ToDoItem.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoItem.swift
+//  ToDoList
+//
+//  Created by John Linnehan on 9/27/21.
+//
+
+import Foundation
+
+
+ struct ToDoItem {
+     var name: String
+     var date: Date
+     var notes: String
+ }

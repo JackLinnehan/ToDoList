eacf1227b25986b56c8b969edb78af3d2e468b2a
JackLinnehan <linnehjb@bc.edu>
2021-09-27 10:08:33 -0400

    5.2

--- ToDoList/ToDoListViewController.swift
+++ ToDoList/ToDoListViewController.swift
@@ -29,8 +29,7 @@ extension ToDoListViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        print("Cell Row format called")
-        print("Test")
+        print("Cell Row format called in extension")
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         cell.textLabel?.text = toDoArray[indexPath.row]
         return cell

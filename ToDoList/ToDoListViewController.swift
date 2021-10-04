bdee3e4a6b45e1b4be2944d84366e12cfef3407e
JackLinnehan <linnehjb@bc.edu>
2021-10-04 12:41:13 -0400

    5.10

--- ToDoList/ToDoListViewController.swift
+++ ToDoList/ToDoListViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ToDoListViewController: UIViewController {
     
@@ -21,6 +22,68 @@ class ToDoListViewController: UIViewController {
         
         loadData()
     }
+    
+    func authorizeLocalNotification() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            guard error == nil else {
+                print("Error \(error!.localizedDescription)")
+                return
+            }
+            if granted {
+                print("Granted")
+            } else {
+                print("denied notification")
+                //TODO: put alert in here telling the user what to do
+            }
+        }
+    }
+    
+    func setNotifications() {
+        guard toDoItems.count > 0 else {
+            return
+        }
+        //remove notifications
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+        
+        //recreate with the saved data
+        for index in 0..<toDoItems.count {
+            if toDoItems[index].reminderSet {
+                let toDoItem = toDoItems[index]
+                toDoItems[index].notificationID = setCalendarNotification(title: toDoItem.name, subtitle: "", body: toDoItem.notes, badgeNumber: nil, sound: .default, date: toDoItem.date)
+            }
+        }
+    }
+    
+    func setCalendarNotification(title: String, subtitle: String, body: String, badgeNumber: NSNumber?, sound: UNNotificationSound?, date: Date) -> String {
+        //create content
+        let content = UNMutableNotificationContent()
+        
+        content.title = title
+        content.subtitle = subtitle
+        content.body = body
+        content.sound = sound
+        content.badge = badgeNumber
+        
+        //create trigger
+        var dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute], from: date)
+        dateComponents.second = 00
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+        
+        //create request
+        let notificationID = UUID().uuidString
+        let request = UNNotificationRequest(identifier: notificationID, content: content, trigger: trigger)
+        
+        //register a request with the notification center
+        UNUserNotificationCenter.current().add(request) { (error) in
+            if let error = error {
+                print("Error")
+            } else {
+                print("notificaiton scheduled")
+            }
+        }
+        return notificationID
+    }
+    
     
     func saveData() {
         let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
